3fef2ea40527bea37e6c638999ea6cbffd4adb30
bowmana <agbowman0001@student.stcc.edu>
2021-12-09 23:20:39 -0500

    testing

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+
